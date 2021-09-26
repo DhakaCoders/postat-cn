610bef381b3244f4fb83562cd2d47451cfbae080
Azizul Haque <azizul.softsw@gmail.com>
2021-09-26 17:50:45 +0600

    front-page

--- themes/postat/enq-scripts/fonts.php
+++ themes/postat/enq-scripts/fonts.php
@@ -1,7 +1,7 @@
 <?php 
 wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/fonts/font-awesome/font-awesome.css', array(), '5.5.0');
-wp_enqueue_style('cbv-fonts-barlow', 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', array(), '1.0.0');
-wp_enqueue_style('cbv-fonts-source', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap', array(), '1.0.0');
+wp_enqueue_style('cbv-fonts-poppins', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', array(), '1.0.0');
+wp_enqueue_style('cbv-fonts-source', 'https://fonts.googleapis.com/css2?family=Epilogue:wght@700&display=swap', array(), '1.0.0');
 wp_enqueue_style('cbv-custom.fonts', get_template_directory_uri() . '/assets/fonts/custom-fonts.css', array(), '1.0.0');
 
 ?>
