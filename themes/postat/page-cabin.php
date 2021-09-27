6dfc3cbc0db7022d711a7761bc4184db655362fd
Azizul Haque <azizul.softsw@gmail.com>
2021-09-27 18:53:40 +0600

    package

---
+++ themes/postat/page-cabin.php
@@ -0,0 +1,7 @@
+<?php 
+/*Template Name: Cabin*/
+get_header();
+$thisID = get_the_ID();
+?>
+
+<?php get_footer(); ?>
---
+++ themes/postat/page-luxury-packages.php
@@ -0,0 +1,7 @@
+<?php 
+/*Template Name: Luxury Packages*/
+get_header();
+$thisID = get_the_ID();
+?>
+
+<?php get_footer(); ?>
