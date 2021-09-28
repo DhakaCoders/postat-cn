808924aaa15f76a34718b0a4263c082d215b2834
Rannojit <cserannojitsarkar22@gmail.com>
2021-09-28 12:54:50 +0600

    .

---
+++ themes/postat/page-facilities-landing.php
@@ -0,0 +1,156 @@
+<?php 
+/*Template Name: Facilities Landing */
+get_header();
+$thisID = get_the_ID();
+?>
+
+
+
+  <section class="page-banner">
+    <div class="page-banner-bg-black"></div>
+    <div class="page-bnr-bg inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/facilities-landing-bnr-bg.jpg');"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="page-bnr-cntlr">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- <div class="gap-50"></div> -->
+
+  <div class="facilites-landing-ctlr">
+    <section class="pt-intro-text-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="pt-intro-text-module">
+              <p>
+                Our luxurious amenities, in harmony with sustainable living, offer the ideal setting for you to disconnect and recharge. Set away from the hustle of city life, our enviable location is only eighty minutes from Sydney.  From spellbinding vistas all to yourself, to our thoughtful and welcoming spaces, POST at Oakgate provides an opportunity to slow down and reconnect.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="lftdes-rgtimg-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="lftdes-rgtimg-sec-inr">
+              <div class="lftdes-rgtimg-grds">
+                <div class="lftdes-rgtimg-grd-item">
+                  <div class="lftdes-rgtimg-ctlr lftimg-rgtdes">
+                    <div class="lftdes-rgtimg-lft">
+                      <div class="lftdes-rgtimg-img inline-bg" style="background:url(<?php echo THEME_URI; ?>/assets/images/lftdes-rgtimg-img-05.jpg)">
+                        <img src="<?php echo THEME_URI; ?>/assets/images/lftdes-rgtimg-img-05.jpg">
+                      </div>
+                    </div>
+                    <div class="lftdes-rgtimg-rgt">
+                      <div class="lftdes-rgtimg-des pt-link-hover">
+                        <h2 class="lftdes-rgtimg-title fl-h2">Waterhole</h2>
+                        <h3 class="lftdes-rgtimg-subtitle fl-h6">Lorem ipsum</h3>
+                        <p>The Waterhole is the estate’s communal guest facility, and is a prime example of our awe-inspiring architectural masterpieces. </p>
+                        <a href="#">Learn more</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="lftdes-rgtimg-grd-item">
+                  <div class="lftdes-rgtimg-ctlr">
+                    <div class="lftdes-rgtimg-lft">
+                      <div class="lftdes-rgtimg-img inline-bg" style="background:url(<?php echo THEME_URI; ?>/assets/images/lftdes-rgtimg-img-06.jpg)">
+                        <img src="<?php echo THEME_URI; ?>/assets/images/lftdes-rgtimg-img-06.jpg">
+                      </div>
+                    </div>
+                    <div class="lftdes-rgtimg-rgt">
+                      <div class="lftdes-rgtimg-des pt-link-hover">
+                        <h2 class="lftdes-rgtimg-title fl-h2">Golf course</h2>
+                        <h3 class="lftdes-rgtimg-subtitle fl-h6">Lorem ipsum</h3>
+                        <p>Ut egestas eros orci, in elementum urna consequat a. Aliquam at rutrum lorem, vel tempus ligula. In dictum lectus at mi ornare, nec tincidunt ligula fermentum. Vestibulum facilisis turpis aliquet ligula egestas faucibus. Etiam congue odio et tincidunt semper. </p>
+                        <a href="#">Learn more</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="lftdes-rgtimg-grd-item">
+                  <div class="lftdes-rgtimg-ctlr lftimg-rgtdes">
+                    <div class="lftdes-rgtimg-lft">
+                      <div class="lftdes-rgtimg-img inline-bg" style="background:url(<?php echo THEME_URI; ?>/assets/images/lftdes-rgtimg-img-07.jpg)">
+                        <img src="<?php echo THEME_URI; ?>/assets/images/lftdes-rgtimg-img-07.jpg">
+                      </div>
+                    </div>
+                    <div class="lftdes-rgtimg-rgt">
+                      <div class="lftdes-rgtimg-des pt-link-hover">
+                        <h2 class="lftdes-rgtimg-title fl-h2">Other amenties</h2>
+                        <h3 class="lftdes-rgtimg-subtitle fl-h6">Lorem ipsum</h3>
+                        <p>There are a range of other activities and facilities, all located onsite for the convenience of our guests and visitors.  </p>
+                        <a href="#">Learn more</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="luxury-sec inline-bg" style="background:url(<?php echo THEME_URI; ?>/assets/images/luxury-bg.jpg)">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="luxury-sec-inr">
+              <div class="luxury-des">
+                <h2 class="luxury-des-title fl-h2"><a href="#">View our luxury packages…</a></h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="follow-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="follow-sec-inr">
+              <div class="follow-des">
+                <h2 class="follow-des-title fl-h2">Follow @postatoakgate</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="follow-img-ctlr">
+        <div class="follow-img">
+          <img src="<?php echo THEME_URI; ?>/assets/images/follow-des-img.png">
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="follow-btm-des">
+              <div class="follow-btm-des-hdr">
+                <h3 class="follow-btm-des-title fl-h6">Immerse yourself in the landscape, Tag us #postatoakgate #cabinporn #immersivelandscapes </h3>
+              </div>
+              <div class="follow-btm-des-btn">
+                <a class="fl-black-btn" href="#">follow @postatoakgate</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+<?php get_footer(); ?>
+
+
+  
