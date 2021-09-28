301f20ab723ea9bdacc57d34c7dcfa66142cfaa2
Rannojit <cserannojitsarkar22@gmail.com>
2021-09-28 12:22:33 +0600

    .

---
+++ themes/postat/pag-cabin-facilities-specific.php
@@ -0,0 +1,263 @@
+<?php 
+/*Template Name: Cabin Facilities Specific */
+get_header();
+$thisID = get_the_ID();
+?>
+
+
+<section class="page-banner">
+    <div class="page-bnr-slider-cntlr">
+      <div class="page-bnr-slider pageBnrSlider">
+        <div class="pg-bnr-slide-item-cntlr">
+          <div class="pg-bnr-slide-item">
+            <div class="page-banner-bg-black"></div>
+            <div class="page-bnr-bg inline-bg" style="background-image: url('assets/images/page-bnr-slider-bg-img-002.jpg');"></div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="page-bnr-cntlr">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pg-bnr-slide-item-cntlr">
+          <div class="pg-bnr-slide-item">
+            <div class="page-banner-bg-black"></div>
+            <div class="page-bnr-bg inline-bg" style="background-image: url('assets/images/page-bnr-slider-bg-img-002.jpg');"></div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="page-bnr-cntlr">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pg-bnr-slide-item-cntlr">
+          <div class="pg-bnr-slide-item">
+            <div class="page-banner-bg-black"></div>
+            <div class="page-bnr-bg inline-bg" style="background-image: url('assets/images/page-bnr-slider-bg-img-002.jpg');"></div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="page-bnr-cntlr">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pg-bnr-slide-item-cntlr">
+          <div class="pg-bnr-slide-item">
+            <div class="page-banner-bg-black"></div>
+            <div class="page-bnr-bg inline-bg" style="background-image: url('assets/images/page-bnr-slider-bg-img-002.jpg');"></div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="page-bnr-cntlr">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pg-bnr-slide-item-cntlr">
+          <div class="pg-bnr-slide-item">
+            <div class="page-banner-bg-black"></div>
+            <div class="page-bnr-bg inline-bg" style="background-image: url('assets/images/page-bnr-slider-bg-img-002.jpg');"></div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="page-bnr-cntlr">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pg-bnr-slide-item-cntlr">
+          <div class="pg-bnr-slide-item">
+            <div class="page-banner-bg-black"></div>
+            <div class="page-bnr-bg inline-bg" style="background-image: url('assets/images/page-bnr-slider-bg-img-002.jpg');"></div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="page-bnr-cntlr">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<div class="cabin-facilities-specific-ctlr">
+
+  <section class="lftdes-rgtimg-sec">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="lftdes-rgtimg-sec-inr">
+            <div class="lftdes-rgtimg-ctlr lftimg-rgtdes">
+              <div class="lftdes-rgtimg-lft">
+                <div class="lftdes-rgtimg-img inline-bg" style="background:url(assets/images/lftdes-rgtimg-img-02.jpg)">
+                  <img src="assets/images/lftdes-rgtimg-img-02.jpg">
+                </div>
+              </div>
+              <div class="lftdes-rgtimg-rgt">
+                <div class="lftdes-rgtimg-des pt-link-hover">
+                  <h2 class="lftdes-rgtimg-title fl-h2">Watagans</h2>
+                  <div class="fea-grd-ctlr">
+                    <div class="fea-item pt-user">
+                      <i>
+                        <svg class="fea-item-img-01-svg" width="34.393" height="43.773" viewBox="0 0 34.393 43.773" fill="#000000">
+                        <use xlink:href="#fea-item-img-01-svg"></use> </svg>
+                      </i>
+                      <span>2</span>
+                    </div>
+                    <div class="fea-item pt-bed">
+                      <i>
+                        <svg class="fea-item-img-02-svg" width="50.661" height="42" viewBox="0 0 50.661 42" fill="#000000">
+                        <use xlink:href="#fea-item-img-02-svg"></use> </svg>
+                      </i>
+                      <span>1</span>
+                    </div>
+                    <div class="fea-item pt-bath">
+                      <i>
+                        <svg class="fea-item-img-03-svg" width="42.47" height="42.3" viewBox="0 0 42.47 42.3" fill="#000000">
+                        <use xlink:href="#fea-item-img-03-svg"></use> </svg>
+                      </i>
+                      <span>1</span>
+                    </div>
+                  </div>
+                  <h3 class="lftdes-rgtimg-subtitle fl-h6">Secluded beauty</h3>
+                  <p>This secluded cabin is the ideal setting for a luxurious and romantic getaway. Watch the sunset beyond the stunning mountain range, as you breathe in the fresh mountain air and feel the embrace of your unspoiled surroundings.</p>
+                  <a href="#">Learn more </a>
+                  <div class="lftdes-rgtimg-des-btn btn-ctlr">
+                    <ul class="reset-list clearfix">
+                      <li><a class="fl-transparent-btn" href="#">make a booking</a></li>
+                      <li><a class="fl-gray-btn" href="#">floor plan</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="pt-intro-text-sec">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="pt-intro-text-module">
+            <p>
+              Our luxurious amenities, in harmony with sustainable living, offer the ideal setting for you to disconnect and recharge. Set away from the hustle of city life, our enviable location is only eighty minutes from Sydney.  From spellbinding vistas all to yourself, to our thoughtful and welcoming spaces, POST at Oakgate provides an opportunity to slow down and reconnect.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="amenities-sec">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="amenities-sec-inr">
+            <div class="pt-grids-sec-entry-hdr">
+              <h2 class="fl-h2 pt-grids-sec-entry-hdr-title">Amenities</h2>
+            </div>
+            <div class="amenities-grds">
+              <ul class="reset-list clearfix">
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-01.svg"></i>
+                    <span>Heated Pool</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-02.svg"></i>
+                    <span>Fireplace and firewood</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-03.svg"></i>
+                    <span>Laundry service</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-04.svg"></i>
+                    <span>Air conditioning</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-01.svg"></i>
+                    <span>Heated Pool</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-02.svg"></i>
+                    <span>Fireplace and firewood</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-03.svg"></i>
+                    <span>Laundry service</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="amenities-grd-item">
+                    <i><img src="assets/images/amenities-img-04.svg"></i>
+                    <span>Air conditioning</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="luxury-sec inline-bg" style="background:url(assets/images/luxury-bg.jpg)">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="luxury-sec-inr">
+            <div class="luxury-des">
+              <h2 class="luxury-des-title fl-h2"><a href="#">View our luxury packages…</a></h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
+
+<?php get_footer(); ?>
+
+  
